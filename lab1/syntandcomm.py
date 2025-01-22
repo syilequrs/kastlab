ee3985cef974f226d3fa8ca4410bad097ab2f3fd
syilequrs <timurkasymzhanov@gmail.com>
2025-01-23 04:30:13 +0500

    synt and com

---
+++ lab1/syntandcomm.py
@@ -0,0 +1,17 @@
+#tab
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+#          print("This won't work")
+x = 5
+y = "Hello, World!"
+#no int x \ string y
+"""
+Dark Souls is a dark fantasy action 
+role-playing game series developed by FromSoftware
+and published by Bandai Namco Entertainment
+"""
