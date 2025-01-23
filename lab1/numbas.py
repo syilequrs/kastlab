92033844e72f00719323b6f849d1a6a716ad119f
syilequrs <timurkasymzhanov@gmail.com>
2025-01-23 19:07:04 +0500

    numbers

---
+++ lab1/numbas.py
@@ -0,0 +1,37 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x)) #<class 'int'>
+print(type(y)) #<class 'float'>
+print(type(z)) #<class 'complex'>
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+#all int
+x = 1.10
+y = 1.0
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+#all float
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+#all float
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
+#all complex
+
+#import random
+#print(random.randrange(1, 10))
